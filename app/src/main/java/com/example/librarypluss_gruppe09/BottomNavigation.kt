263f9cf46a1e3341e580c82ca575838d929c21ac
PerspectivCollection <89631433+PerspectivCollection@users.noreply.github.com>
2023-10-09 15:10:56 +0200

    some comanents

--- app/src/main/java/com/example/librarypluss_gruppe09/BottomNavigation.kt
+++ app/src/main/java/com/example/librarypluss_gruppe09/BottomNavigation.kt
@@ -1,6 +1,8 @@
 package com.example.librarypluss_gruppe09
 
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -12,10 +14,12 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.librarypluss_gruppe09.models.ConstantsNavigation
 import com.example.librarypluss_gruppe09.ui.theme.Pink40
+import kotlinx.coroutines.selects.select
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -26,6 +30,7 @@ fun BottomNavigationBar() {
     val current = navBtweenItems?.destination
 
     Scaffold(
+
         topBar = {
             TopAppBar(
                 title = { Text(text = current?.route.toString()) },
@@ -40,9 +45,13 @@ fun BottomNavigationBar() {
                     ConstantsNavigation.BottomNavItems.forEach { screen ->
                         IconButton(onClick = { navController.navigate(screen.route)}) {
                             Icon(
+
                                 screen.icon,
-                                contentDescription = screen.label
+                                contentDescription = screen.label,
+                                //last
+                                modifier = Modifier.padding().fillMaxSize()
                             )
+                            Text(screen.label)
                         }
                     }
                 },
--- app/src/main/java/com/example/librarypluss_gruppe09/models/ConstantsNavigation.kt
+++ app/src/main/java/com/example/librarypluss_gruppe09/models/ConstantsNavigation.kt
@@ -6,11 +6,13 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.ui.graphics.vector.ImageVector
+import kotlinx.coroutines.selects.select
 
 data class BottomNavItem(
     val label: String,
     val icon: ImageVector,
     val route: String,
+    //add selected
 )
 
 object ConstantsNavigation {
