c5b11871581c6fe46b2e3b3abf7823e9eda86367
heljark <111959005+heljark@users.noreply.github.com>
2023-10-10 22:45:27 +0200

    enkel profile forsjlag

---
+++ app/src/main/java/com/example/librarypluss_gruppe09/Profile.kt
@@ -0,0 +1,34 @@
+package com.example.librarypluss_gruppe09
+
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Preview
+@Composable
+fun Profile(){
+    Column (modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally){
+        Image(
+            painter = painterResource(R.drawable.ic_launcher_foreground),
+            contentDescription = "profile picture",
+            modifier = Modifier
+                .size(100.dp)
+                .clip(CircleShape)
+        )
+        Text(text = "user name")
+        Text(text = "settings")
+        Text(text = "statestikk")
+    }
+
+}
