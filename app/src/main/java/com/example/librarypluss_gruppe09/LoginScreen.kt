846ddd74fefd7c245625acf65908e43e30704728
PerspectivCollection <89631433+PerspectivCollection@users.noreply.github.com>
2023-10-10 15:42:12 +0200

    fixed login

--- app/src/main/java/com/example/librarypluss_gruppe09/LoginScreen.kt
+++ app/src/main/java/com/example/librarypluss_gruppe09/LoginScreen.kt
@@ -7,28 +7,29 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.tooling.preview.Preview
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun LoginScreen(goto: () -> Unit,
+fun LoginScreen(
+    goto: () -> Unit,
     modifier: Modifier = Modifier
 ) {
     var password = ""
 
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
     ) {
         OutlinedTextField(value = "Username", onValueChange = {})
         OutlinedTextField(
             value = password,
-            onValueChange = { password = it},
+            onValueChange = { password = it },
             visualTransformation = PasswordVisualTransformation(),
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
         )
