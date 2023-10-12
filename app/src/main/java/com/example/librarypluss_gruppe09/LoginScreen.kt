d86435fb34d2f48d92688a8b987f464e25d27a16
PerspectivCollection <89631433+PerspectivCollection@users.noreply.github.com>
2023-10-12 09:25:55 +0200

    fillmaxe one login class

--- app/src/main/java/com/example/librarypluss_gruppe09/LoginScreen.kt
+++ app/src/main/java/com/example/librarypluss_gruppe09/LoginScreen.kt
@@ -2,21 +2,22 @@ package com.example.librarypluss_gruppe09
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
-
+import androidx.compose.ui.tooling.preview.Preview
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun LoginScreen(
-    goto: () -> Unit,
+fun LoginScreen(goto: () -> Unit,
     modifier: Modifier = Modifier
 ) {
     var password = ""
@@ -24,12 +25,12 @@ fun LoginScreen(
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
-        modifier = modifier
+        modifier = modifier.fillMaxSize()
     ) {
         OutlinedTextField(value = "Username", onValueChange = {})
         OutlinedTextField(
             value = password,
-            onValueChange = { password = it },
+            onValueChange = { password = it},
             visualTransformation = PasswordVisualTransformation(),
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
         )
