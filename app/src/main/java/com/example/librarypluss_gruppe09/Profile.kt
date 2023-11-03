1265852e87c56c450ba774dbbe4fab9dfe0b225a
heljark <111959005+heljark@users.noreply.github.com>
2023-11-03 19:19:40 +0100

    updated profile clas

--- app/src/main/java/com/example/librarypluss_gruppe09/Profile.kt
+++ app/src/main/java/com/example/librarypluss_gruppe09/Profile.kt
@@ -6,19 +6,21 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
-@Preview
+
+
 @Composable
 fun Profile(){
     Column (modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally){
+        Text(text = "user name")
         Image(
             painter = painterResource(R.drawable.ic_launcher_foreground),
             contentDescription = "profile picture",
@@ -26,9 +28,18 @@ fun Profile(){
                 .size(100.dp)
                 .clip(CircleShape)
         )
-        Text(text = "user name")
-        Text(text = "settings")
-        Text(text = "statestikk")
+
+        Button(onClick = { }) {
+            Text(text = "Settings")
+        }
+        Button(onClick = {  }) {
+            Text(text = "Statestikk")
+        }
+        Button(onClick = {  }) {
+            Text(text = "Goals")
+        }
+
     }
 
 }
+
