72145e3c61b3521721a14a163d2278f99a8807f7
heljark <111959005+heljark@users.noreply.github.com>
2023-11-22 14:19:26 +0100

    added viewmoden

---
+++ app/src/main/java/com/example/librarypluss_gruppe09/screen/profile/ProfileViewModel.kt
@@ -0,0 +1,16 @@
+package com.example.librarypluss_gruppe09.screen.profile
+
+import androidx.lifecycle.ViewModel
+import com.example.librarypluss_gruppe09.models.Media
+import javax.inject.Inject
+
+
+class ProfileViewModel @Inject
+constructor(private val media: Media) : ViewModel() {
+
+    fun mediaListUpdate(){
+        val names = mutableListOf<String>()
+        names.add(media.tittle)
+
+    }
+}
